2e30971dbc1509ca1ac1cce410badf801050f08d
notyusta <xabhista19@gmail.com>
2025-09-28 15:33:03 +0700

    feat: update api url

--- server.lua
+++ server.lua
@@ -1,4 +1,4 @@
-local webURL = "https://cdn-fivem.raznar.net"
+local webURL = "https://cdn-fivem-api.raznar.net"
 local function clearCache()
   print("clearing cache!")
   local url = string.format("%s/api/client/cdn/clear/%s?key=%s", webURL, Config.id, Config.key)
