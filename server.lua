d1a35e0aea8514bec8bc9784815b97772c4941f6
notyusta <xabhista19@gmail.com>
2025-10-28 16:32:05 +0700

    feat: update api url

--- server.lua
+++ server.lua
@@ -1,7 +1,7 @@
-local webURL = "https://cdn-fivem-api.raznar.net"
+
 local function clearCache()
   print("clearing cache!")
-  local url = string.format("%s/api/client/cdn/clear/%s?key=%s", webURL, Config.id, Config.key)
+  local url = string.format("%s/api/client/cdn/clear/%s?key=%s", Config.api_url, Config.id, Config.key)
   PerformHttpRequest(url, function(status, _data) 
     print("result status: " .. status)
   end, "DELETE")
