c39dbf9fb508676aaf562c5df5bf1ef33e57fb9d
notyusta <xabhista19@gmail.com>
2025-10-29 17:19:49 +0700

    feat: update config

--- server.lua
+++ server.lua
@@ -1,19 +1,25 @@
-
-local function clearCache()
-  print("clearing cache!")
-  local url = string.format("%s/api/client/cdn/clear/%s?key=%s", Config.api_url, Config.id, Config.key)
-  PerformHttpRequest(url, function(status, _data) 
-    print("result status: " .. status)
-  end, "DELETE")
+local function urlencode(str)
+    if str then
+        str = str:gsub("\n", "\r\n")
+        str = str:gsub("([^%w%-_%.~])", function(c)
+            return string.format("%%%02X", string.byte(c))
+        end)
+    end
+    return str
 end
 
-AddEventHandler('onResourceStart', function(resourceName)
-  if (GetCurrentResourceName() ~= resourceName) then
-    return
-  end
+local function clearCache()
+    print("clearing cache!")
 
-  clearCache()
-end)
+    local id  = urlencode(Config.id)
+    local key = urlencode(Config.key)
+
+    local url = string.format("%s/api/client/cdn/clear/%s?key=%s", Config.api_url, id, key)
+
+    PerformHttpRequest(url, function(status, _data)
+        print("result status: " .. status)
+    end, "DELETE")
+end
 
 
 AddEventHandler('onResourceStop', function(resourceName)
